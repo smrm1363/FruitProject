70f086d711452efcf1d470473b174fbc8e7d6d84
Mohammadreza <smrm1363@gmail.com>
2019-03-26 15:48:45 +0430

    changes

---
+++ src/test/java/com/cybercom/fruitstore/domain/Fruit/FruitServiceTest.java
@@ -0,0 +1,22 @@
+package com.cybercom.fruitstore.domain.Fruit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+public class FruitServiceTest {
+
+    @Test
+    public void saveOrUpdate() {
+
+    }
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+}
